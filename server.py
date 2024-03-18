426641fb738a2cdda97de5658083a7fa4557cc71
mayllon <87451748+m3mayllon@users.noreply.github.com>
2024-03-18 13:21:57 -0700

    chore: linting

--- server.py
+++ server.py
@@ -1,29 +1,29 @@
 from flask import Flask, render_template, request
 # Import the Maths package here
 
-app = Flask("Mathematics Problem Solver")
+app = Flask('Mathematics Problem Solver')
 
-@app.route("/sum")
+@app.route('/sum')
 def sum_route():
     num1 = float(request.args.get('num1'))
     num2 = float(request.args.get('num2'))
     # Write your code here
 
-@app.route("/sub")
+@app.route('/sub')
 def sub_route():
     num1 = float(request.args.get('num1'))
     num2 = float(request.args.get('num2'))
     # Write your code here
 
-@app.route("/mul")
+@app.route('/mul')
 def mul_route():
     num1 = float(request.args.get('num1'))
     num2 = float(request.args.get('num2'))
     # Write your code here  
 
-@app.route("/")
+@app.route('/') 
 def render_index_page():
     # Write your code here
     
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
