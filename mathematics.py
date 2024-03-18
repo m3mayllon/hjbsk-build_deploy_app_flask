93a9dee26ef95a3189df79c8945c3b1fa9dda586
mayllon <87451748+m3mayllon@users.noreply.github.com>
2024-03-18 13:42:36 -0700

    feat: create maths, update math routers

---
+++ mathematics.py
@@ -0,0 +1,10 @@
+class Maths:
+
+    def summation(a: float, b: float) -> float:
+        return a + b
+
+    def subtraction(a: float, b: float) -> float:
+        return a - b
+
+    def multiplication(a: float, b: float) -> float:
+        return a * b
--- server.py
+++ server.py
@@ -1,29 +1,38 @@
-from flask import Flask, render_template, request
-# Import the Maths package here
+from flask import Flask, request, render_template
+from mathematics import Maths
+
 
 app = Flask('Mathematics Problem Solver')
+
+
+@app.route('/')
+def render_index_page():
+    return render_template('index.html')
+
 
 @app.route('/sum')
 def sum_route():
     num1 = float(request.args.get('num1'))
     num2 = float(request.args.get('num2'))
-    # Write your code here
+    result = Maths.summation(num1, num2)
+    return str(result), 200
+
 
 @app.route('/sub')
 def sub_route():
     num1 = float(request.args.get('num1'))
     num2 = float(request.args.get('num2'))
-    # Write your code here
+    result = Maths.subtraction(num1, num2)
+    return str(result), 200
+
 
 @app.route('/mul')
 def mul_route():
     num1 = float(request.args.get('num1'))
     num2 = float(request.args.get('num2'))
-    # Write your code here  
+    result = Maths.multiplication(num1, num2)
+    return str(result), 200
 
-@app.route('/') 
-def render_index_page():
-    # Write your code here
-    
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080, debug=True)
